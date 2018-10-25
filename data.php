0fea901dbb70d18ef71ecf87724fca71d330005f
Ida Hallner <42939733+idahal@users.noreply.github.com>
2018-10-25 08:55:56 +0200

    Add array

--- data.php
+++ data.php
@@ -1,4 +1,117 @@
 <?php
 declare(strict_types=1);
-// This is the file where you can keep your data arrays such as articles and
-// authors.
+
+    $news = [
+        [
+          'title' =>'Efectuar lo acordado',
+          'author' =>[
+            'id' => 5,
+            'name' => 'Esther Acebo',
+          ],
+          'date' => '05/02/2017',
+          'img'=> 'https://images.unsplash.com/photo-1534322904425-1bca0e9211e8?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=13eca4d401fd7b5fac7239fb81671147&auto=format&fit=crop&w=600&q=60',
+          'article' => "After a botched robbery, Tokyo is recruited by The Professor to join his crew in order to steal billions in Euros from the European Union Mint. On the day of the heist, things don't go as planned.",
+          'like' => '15'
+        ],
+        [
+          'title' =>'Errar al disparar',
+          'author' =>[
+            'id' => 1,
+            'name' => 'Alex Rodrigo',
+          ],
+          'date' => '05/16/2017',
+          'img'=> 'https://images.unsplash.com/photo-1517292987719-0369a794ec0f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=79cbb47c2fa1dab8479b31a61567638a&auto=format&fit=crop&w=600&q=60',
+          'article' => "The police catch the image of one of the robbers. Raquel suspects the gentle gentleman she met in the bar.",
+          'like' => '5'
+        ],
+        [
+          'title' =>'Imprudencias letales',
+          'author' =>[
+            'id' => 2,
+            'name' => 'Úrsula Corberó',
+          ],
+          'date' => '05/09/2017',
+          'img'=> 'https://images.unsplash.com/photo-1502465771179-51f3535da42c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=82edcc2528772a41ea96fc6f3ce9a798&auto=format&fit=crop&w=600&q=60',
+          'article' =>"Raquel, who is running the police operation and handling the hostage negotiations, makes contact with the Professor. One of the hostages turns out to be crucial to his plans.",
+          'like' => '10',
+        ],
+        [
+          'title' =>'Caballo de Troya',
+          'author' =>[
+            'id' => 3,
+            'name' => 'Alba Flores',
+          ],
+          'date' => '05/23/2017',
+          'img'=> 'https://images.unsplash.com/photo-1521798465888-a81a85e00ed7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1c06286f809f98da062fdd045ba2cf9f&auto=format&fit=crop&w=600&q=60',
+          'article' => "Raquel is adversely affected by a personal crisis with her ex-husband. The hostages are afraid of the sound of gunshots they heard. Thieves argue among themselves.",
+          'like' => '17'
+        ],
+        [
+          'title' =>'La cálida Guerra Fría',
+          'author' =>[
+            'id' => 3,
+            'name' => 'Alba Flores',
+          ],
+          'date' => '06/06/2017',
+          'img'=> 'https://images.unsplash.com/photo-1476242906366-d8eb64c2f661?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=56490213cac9e45b868b625a4f143eeb&auto=format&fit=crop&w=600&q=60',
+          'article' => "The problems inside and outside the National Currency and Stamp Factory are growing. The biggest robbery in history does not allow any mistakes and each time, the tension and fear is rising. What opportunities are there for everything to go well?",
+          'like' => '2'
+        ],
+        [
+          'title' =>'El día de la marmota',
+          'author' =>[
+            'id' => 2,
+            'name' => 'Úrsula Corberó',
+          ],
+          'date' => '05/30/2017',
+          'img'=> 'https://images.unsplash.com/photo-1517061493161-6f312d1c36d0?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=caac4340d46f8dcaecae907d6b505d6d&auto=format&fit=crop&w=600&q=60',
+          'article' => "A sudden development in the investigation and a mistake made by one of the thieves confronts the Professor with a serious risk that could lead to his identity being revealed.",
+          'like' => '12'
+        ],
+        [
+          'title' =>'El que la sigue la consigue',
+          'author' =>[
+            'id' => 4,
+            'name' => 'Jaime Lorente',
+          ],
+          'date' => '06/27/2017',
+          'img'=> 'https://images.unsplash.com/photo-1508921340878-ba53e1f016ec?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3024338c801ac09e660b66e1cd5adb40&auto=format&fit=crop&w=600&q=60',
+          'article' => "The professor talks with Raquel's mother on stressful conditions. Darphanede wants thieves to choose between money and freedom from the hostages.",
+          'like' => '7'
+        ],
+        [
+          'title' =>'Refrigerada inestabilidad',
+          'author' =>[
+            'id' => 4,
+            'name' => 'Jaime Lorente',
+          ],
+          'date' => '06/13/2017',
+          'img'=> 'https://images.unsplash.com/photo-1526540976851-f136023efba9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b3ef2bc7183e8c32a614c187da4dcf10&auto=format&fit=crop&w=600&q=60',
+          'article' => "A new police plan will make Raquel enter the National Factory of Currency and Stamp, right at the peak of the biggest heist in history. Berlin will be responsible for being the most extravagant host that the inspector has ever seen. What is their plan?",
+          'like' => '5'
+        ],
+        [
+          'title' =>'Bella ciao',
+          'author' =>[
+            'id' => 5,
+            'name' => 'Esther Acebo',
+          ],
+          'date' => '11/23/2017',
+          'img'=> 'https://images.unsplash.com/photo-1521710385092-ffb18195a459?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=eb3acf37817cb8ac64708b3726a3d777&auto=format&fit=crop&w=600&q=60',
+          'article' => "The police are storming into the National House of Stamp and Currency. Will the hostages be saved and can the robbers escape?",
+          'like' => '18'
+        ],
+        [
+          'title' =>'La cabeza del plan',
+          'author' =>[
+            'id' => 1,
+            'name' => 'Alex Rodrigo',
+          ],
+          'date' => '10/23/2017',
+          'img'=> 'https://images.unsplash.com/photo-1534237886190-ced735ca4b73?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c56c911a0c3085ab56d1b1840960e7cf&auto=format&fit=crop&w=600&q=60',
+          'article' => "Tokyo is questioned by the police. The Professor and Raquel's ex-husband are getting into a fight.",
+          'like' => '10'
+        ],
+      ];
+?>
+
