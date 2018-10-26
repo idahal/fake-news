97f57452bd16c00102cc90487ce2e324f432e696
Ida Hallner <42939733+idahal@users.noreply.github.com>
2018-10-26 10:44:16 +0200

    Add footer

---
+++ footer.php
@@ -0,0 +1,27 @@
+<!---footer-->
+<div class="footer">
+		<div class="footer_box1">
+				<h5>Contact: </h5>
+				<img src="./image/mail.png"> fake@email.se
+				<br>
+				<br>
+				<img src="./image/phone.png"> 0762-377790
+				<br>
+				<br>
+				<img src="./image/skype.png"> FakeNews
+				<br>
+				<br>
+				<p>&copy; Ida Hallner 2018</p>
+	 </div>
+	 <div class="footer_box2">
+				<h5>Follow us: </h5>
+				<img src="./image/facebook.png">
+				<img src="./image/instagram.png">
+				<img src="./image/pintrest.png">
+				<img src="./image/twitter.png">
+				<img src="./image/youtube.png">
+		</div>
+</div> <!--footer-->
+
+</body>
+</html>
