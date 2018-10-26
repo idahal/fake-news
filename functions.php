5bd8d1692f4c7839c58b3b6c3a98a2331ee36e5d
Ida Hallner <42939733+idahal@users.noreply.github.com>
2018-10-26 10:38:34 +0200

    Add functions

--- functions.php
+++ functions.php
@@ -1,4 +1,29 @@
 <?php
 declare(strict_types=1);
-// This is the file where you can keep all your functions. Remember to NOT
-// execute/run any functions in this file. Keep it dumb.
+/**
+ * Sort article based on date.
+ * @param  array $a
+ * @param  array $b
+ * @return int
+ */
+function sortFunction(array $a,array $b ): int {
+   return strtotime($b['date']) - strtotime($a['date']);
+}
+
+/**
+ * Get the articel with most likes.
+ * @param  array $news
+ * @return array
+ */
+function getMax(array $news): array {
+    $max = 0;
+    foreach( $news as $key => $value ) {
+      $max = max(array($max, $value['like']));
+    }
+      foreach ($news as $key => $value) {
+        if ($value['like'] == $max) {
+          return $value;
+      }
+    }
+  }
+
