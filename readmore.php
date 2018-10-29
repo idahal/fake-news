9053c8a644ed00a5d5f91d307141363d9d46a9a8
Ida Hallner <42939733+idahal@users.noreply.github.com>
2018-10-29 20:13:09 +0100

    Add readmore.

---
+++ readmore.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+require(__DIR__.'/header.php');
+?>
+
+<div class="alert alert-dismissible alert-primary">
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+  <strong>Sorry something went wrong, <strong> <a href="index.php" class="alert-link">press here to go back.</a>
+</div>
